9b82ab1ad03c758b6f33e1e5ff6a2b73ff68fccc
9seconds <nineseconds@yandex.ru>
2016-03-06 23:50:47 +0300

    Add tests for get/split indent

--- tests/test_core_lexer.py
+++ tests/test_core_lexer.py
@@ -33,3 +33,24 @@ def test_clean_line(input_, output_):
 ))
 def test_reindent_line(input_, output_):
     assert lexer.reindent_line(input_) == output_
+
+
+@pytest.mark.parametrize("indent_", (
+    "",
+    " ",
+    "    ",
+    "\t",
+    "\t\t",
+    "\t \t",
+    "\t\t ",
+    " \t\t"
+))
+@pytest.mark.parametrize("content_", (
+    "",
+    "a"
+))
+def test_get_split_indent(indent_, content_):
+    text = indent_ + content_
+
+    assert lexer.get_indent(text) == indent_
+    assert lexer.split_indent(text) == (indent_, content_)
