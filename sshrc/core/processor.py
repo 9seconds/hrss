26f71f5ca8f800724e32e7d2298bb34f287b0346
9seconds <nineseconds@yandex.ru>
2016-03-11 09:41:45 +0300

    Add tests for processor

--- sshrc/core/processor.py
+++ sshrc/core/processor.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
 
 
-import sshrc.core.generator
 import sshrc.core.lexer
 import sshrc.core.parser
 
 
-def parse(content):
+def process(content):
     content = content.split("\n")
     content = sshrc.core.lexer.lex(content)
     content = sshrc.core.parser.parse(content)
-    content = sshrc.core.generator.generate(content)
+    content = generate(content)
     content = "\n".join(content)
 
     return content
+
+
+def generate(tree):
+    for host in flat(tree):
+        yield "Host {}".format(host.fullname)
+        for option, value in sorted(host.options.items()):
+            yield "    {} {}".format(option, value)
+        yield ""
+
+
+def flat(tree):
+    for host in sorted(tree.childs, key=lambda h: (h.name == "*", h.name)):
+        yield from flat_host_data(host)
+
+
+def flat_host_data(tree):
+    for host in tree.hosts:
+        yield from flat_host_data(host)
+    if tree.trackable:
+        yield tree
---
+++ tests/test_processor.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+
+import pytest
+
+import sshrc.core.lexer as lexer
+import sshrc.core.parser as parser
+import sshrc.core.processor as process
+
+
+CONTENT = """\
+Compression yes
+
+Host q
+    Port 22
+
+    -Host e
+        Protocol 2
+
+        Host h
+            HostName hew
+
+    Host q
+        HostName qqq
+""".strip()
+
+
+def test_generate():
+    tokens = lexer.lex(CONTENT.split("\n"))
+    tree = parser.parse(tokens)
+    new_config = list(process.generate(tree))
+
+    assert new_config == [
+        "Host qeh",
+        "    HostName hew",
+        "    Port 22",
+        "    Protocol 2",
+        "",
+        "Host qq",
+        "    HostName qqq",
+        "    Port 22",
+        "",
+        "Host q",
+        "    Port 22",
+        "",
+        "Host *",
+        "    Compression yes",
+        ""]
+
+def test_process():
+    content = process.process(CONTENT)
+
+    assert process.process(CONTENT) == """\
+Host qeh
+    HostName hew
+    Port 22
+    Protocol 2
+
+Host qq
+    HostName qqq
+    Port 22
+
+Host q
+    Port 22
+
+Host *
+    Compression yes
+"""
